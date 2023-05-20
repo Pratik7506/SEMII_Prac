43a99f32eaae53e79defb4c56f84659ad275b621
Harivansh Thakur <71117889+Harivansh-coder@users.noreply.github.com>
2023-05-20 13:00:34 +0530

    Update crawler.py

--- WebMin/crawler.py
+++ WebMin/crawler.py
@@ -3,6 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+# gets all the links from a given url
 def get_links(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -13,18 +14,20 @@ def get_links(url):
             links.append('https://en.wikipedia.org' + href)
     return links
 
+# searches for a given keyword and returns the top 10 results
 def search(keyword):
     url = f'https://en.wikipedia.org/w/index.php?title=Special:Search&limit=10&offset=0&ns0=1&search={keyword}'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     results = []
-    for link in soup.find_all('a'):
-        href = link.get('href')
+    for result in soup.find_all('div', {'class': 'mw-search-result-heading'}):
+        href = result.find('a').get('href')
         if href and href.startswith('/wiki/') and ':' not in href:
             url = 'https://en.wikipedia.org' + href
             results.append(url)
     return results
 
+# crawls the web starting from a given url and up to a given depth
 def crawl(start_url, max_depth):
     visited_urls = set()
     urls_to_visit = [(start_url, 0)]
@@ -38,10 +41,12 @@ def crawl(start_url, max_depth):
         for link in links:
             urls_to_visit.append((link, depth + 1))
 
+# main function
 if __name__ == '__main__':
     keyword = 'python'
     max_depth = 2
     results = search(keyword)
+    
     for result in results:
         print(f'Search result: {result}')
         crawl(result, max_depth)
